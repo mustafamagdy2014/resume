ba8fff3809880e6c283e52a31517d34365fa5f63
Mustafa Magdy Ahmed Fouad <CSDVMUS@cio.gov.bh>
2016-11-08 09:00:50 +0300

    mustafamagdy

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('views/index');
+  response.sendFile("index.html");
 });
 
 app.listen(app.get('port'), function() {
