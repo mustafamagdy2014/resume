f0af4d4fc5468ed0577e6a149cd89bbe1113a5dd
Mustafa Magdy Ahmed Fouad <CSDVMUS@cio.gov.bh>
2016-11-09 10:39:02 +0300

    mustafamagdy

---
+++ views/pages/js/instagram.js
@@ -0,0 +1,7 @@
+
+    var feed = new Instafeed({
+        get: 'user',
+        userId: 2869390618,
+        accessToken: '2869390618.1677ed0.54bba68cfed34b0590b7c300689416d3'
+    });
+    feed.run();
